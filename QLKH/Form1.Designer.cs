6d2383c59195bc22e500bd4b56facae370a0a5af
HoangQuocDuy29 <quocduyhoang42@gmail.com>
2024-11-13 14:19:12 +0700

    f

---
+++ QLKH/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace QLKH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtmkh = new System.Windows.Forms.TextBox();
+            this.txtht = new System.Windows.Forms.TextBox();
+            this.txtdc = new System.Windows.Forms.TextBox();
+            this.txtdt = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.them = new System.Windows.Forms.Button();
+            this.sua = new System.Windows.Forms.Button();
+            this.xoa = new System.Windows.Forms.Button();
+            this.timkiem = new System.Windows.Forms.Button();
+            this.thoat = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(221, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "THÔNG TIN KHÁCH HÀNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Thông tin chi tiết khách hàng";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mã KH";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Họ tên";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Giới tính";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(428, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Địa chỉ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(428, 155);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Điện thoại";
+            // 
+            // txtmkh
+            // 
+            this.txtmkh.Location = new System.Drawing.Point(116, 107);
+            this.txtmkh.Name = "txtmkh";
+            this.txtmkh.Size = new System.Drawing.Size(121, 22);
+            this.txtmkh.TabIndex = 7;
+            // 
+            // txtht
+            // 
+            this.txtht.Location = new System.Drawing.Point(116, 155);
+            this.txtht.Name = "txtht";
+            this.txtht.Size = new System.Drawing.Size(121, 22);
+            this.txtht.TabIndex = 8;
+            // 
+            // txtdc
+            // 
+            this.txtdc.Location = new System.Drawing.Point(527, 107);
+            this.txtdc.Name = "txtdc";
+            this.txtdc.Size = new System.Drawing.Size(141, 22);
+            this.txtdc.TabIndex = 10;
+            // 
+            // txtdt
+            // 
+            this.txtdt.Location = new System.Drawing.Point(527, 155);
+            this.txtdt.Name = "txtdt";
+            this.txtdt.Size = new System.Drawing.Size(141, 22);
+            this.txtdt.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ",
+            "Khác"});
+            this.comboBox1.Location = new System.Drawing.Point(116, 200);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // them
+            // 
+            this.them.Location = new System.Drawing.Point(45, 246);
+            this.them.Name = "them";
+            this.them.Size = new System.Drawing.Size(75, 23);
+            this.them.TabIndex = 13;
+            this.them.Text = "Thêm";
+            this.them.UseVisualStyleBackColor = true;
+            this.them.Click += new System.EventHandler(this.them_Click);
+            // 
+            // sua
+            // 
+            this.sua.Location = new System.Drawing.Point(175, 246);
+            this.sua.Name = "sua";
+            this.sua.Size = new System.Drawing.Size(75, 23);
+            this.sua.TabIndex = 14;
+            this.sua.Text = "Sửa";
+            this.sua.UseVisualStyleBackColor = true;
+            this.sua.Click += new System.EventHandler(this.sua_Click);
+            // 
+            // xoa
+            // 
+            this.xoa.Location = new System.Drawing.Point(310, 246);
+            this.xoa.Name = "xoa";
+            this.xoa.Size = new System.Drawing.Size(75, 23);
+            this.xoa.TabIndex = 15;
+            this.xoa.Text = "Xoá";
+            this.xoa.UseVisualStyleBackColor = true;
+            this.xoa.Click += new System.EventHandler(this.xoa_Click);
+            // 
+            // timkiem
+            // 
+            this.timkiem.Location = new System.Drawing.Point(447, 246);
+            this.timkiem.Name = "timkiem";
+            this.timkiem.Size = new System.Drawing.Size(75, 23);
+            this.timkiem.TabIndex = 16;
+            this.timkiem.Text = "Tìm Kiếm";
+            this.timkiem.UseVisualStyleBackColor = true;
+            this.timkiem.Click += new System.EventHandler(this.timkiem_Click);
+            // 
+            // thoat
+            // 
+            this.thoat.Location = new System.Drawing.Point(593, 246);
+            this.thoat.Name = "thoat";
+            this.thoat.Size = new System.Drawing.Size(75, 23);
+            this.thoat.TabIndex = 17;
+            this.thoat.Text = "Thoát";
+            this.thoat.UseVisualStyleBackColor = true;
+            this.thoat.Click += new System.EventHandler(this.thoat_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(45, 287);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(143, 16);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Danh sách khách hàng";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(48, 327);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(912, 195);
+            this.listView1.TabIndex = 19;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 534);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.thoat);
+            this.Controls.Add(this.timkiem);
+            this.Controls.Add(this.xoa);
+            this.Controls.Add(this.sua);
+            this.Controls.Add(this.them);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtdt);
+            this.Controls.Add(this.txtdc);
+            this.Controls.Add(this.txtht);
+            this.Controls.Add(this.txtmkh);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtmkh;
+        private System.Windows.Forms.TextBox txtht;
+        private System.Windows.Forms.TextBox txtdc;
+        private System.Windows.Forms.TextBox txtdt;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button them;
+        private System.Windows.Forms.Button sua;
+        private System.Windows.Forms.Button xoa;
+        private System.Windows.Forms.Button timkiem;
+        private System.Windows.Forms.Button thoat;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ QLKH/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLKH
+{
+    public partial class Form1 : Form
+    {
+        List<KhachHang> danhSachKhachHang = new List<KhachHang>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(FormKhachHang_Load); 
+        }
+        private void SaveData()
+        {
+            using (FileStream fs = new FileStream("khachhang.dat", FileMode.Create))
+            using (BinaryWriter writer = new BinaryWriter(fs))
+            {
+                foreach (var kh in danhSachKhachHang)
+                {
+                    writer.Write(kh.MaKH);
+                    writer.Write(kh.HoTen);
+                    writer.Write(kh.GioiTinh);
+                    writer.Write(kh.DiaChi);
+                    writer.Write(kh.DienThoai);
+                }
+            }
+        }
+
+        private void LoadData()
+        {
+            if (File.Exists("khachhang.dat"))
+            {
+                using (FileStream fs = new FileStream("khachhang.dat", FileMode.Open))
+                using (BinaryReader reader = new BinaryReader(fs))
+                {
+                    danhSachKhachHang.Clear(); 
+
+                    while (fs.Position < fs.Length)
+                    {
+                        KhachHang kh = new KhachHang
+                        {
+                            MaKH = reader.ReadString(),
+                            HoTen = reader.ReadString(),
+                            GioiTinh = reader.ReadString(),
+                            DiaChi = reader.ReadString(),
+                            DienThoai = reader.ReadString()
+                        };
+                        danhSachKhachHang.Add(kh); 
+                    }
+                }
+            }
+        }
+
+        private void FormKhachHang_Load(object sender, EventArgs e)
+        {
+
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("Mã KH", 100);
+            listView1.Columns.Add("Họ tên", 150);
+            listView1.Columns.Add("Giới tính", 100);
+            listView1.Columns.Add("Địa chỉ", 200);
+            listView1.Columns.Add("Điện thoại", 120);
+
+
+            LoadData();
+            HienThiListView();
+        }
+
+
+        private void HienThiListView()
+        {
+            listView1.Items.Clear(); 
+
+            foreach (var kh in danhSachKhachHang)
+            {
+                ListViewItem item = new ListViewItem(kh.MaKH); 
+                item.SubItems.Add(kh.HoTen);  
+                item.SubItems.Add(kh.GioiTinh);  
+                item.SubItems.Add(kh.DiaChi);  
+                item.SubItems.Add(kh.DienThoai); 
+
+                listView1.Items.Add(item); 
+            }
+        }
+
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void them_Click(object sender, EventArgs e)
+        {
+            
+            if (danhSachKhachHang.Any(khach => khach.MaKH == txtmkh.Text))
+            {
+                MessageBox.Show("Mã khách hàng đã tồn tại!");
+                return;
+            }
+
+            
+            KhachHang kh = new KhachHang
+            {
+                MaKH = txtmkh.Text,
+                HoTen = txtht.Text,
+                GioiTinh = comboBox1.Text,
+                DiaChi = txtdc.Text,
+                DienThoai = txtdt.Text
+            };
+
+            danhSachKhachHang.Add(kh);
+            HienThiListView();  
+        }
+
+
+        private void xoa_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string maKH = listView1.SelectedItems[0].Text;
+                var kh = danhSachKhachHang.FirstOrDefault(k => k.MaKH == maKH);
+
+                if (kh != null)
+                {
+                    danhSachKhachHang.Remove(kh);
+                    HienThiListView();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn khách hàng để xóa!");
+            }
+        }
+
+        private void sua_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string maKH = listView1.SelectedItems[0].Text;
+                var kh = danhSachKhachHang.FirstOrDefault(k => k.MaKH == maKH);
+
+                if (kh != null)
+                {
+                    kh.HoTen = txtht.Text;
+                    kh.GioiTinh = comboBox1.Text;
+                    kh.DiaChi = txtdc.Text;
+                    kh.DienThoai = txtdt.Text;
+                    HienThiListView();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn khách hàng để sửa!");
+            }
+        }
+
+        private void timkiem_Click(object sender, EventArgs e)
+        {
+            string maKH = txtmkh.Text;
+            var kh = danhSachKhachHang.FirstOrDefault(k => k.MaKH == maKH);
+
+            if (kh != null)
+            {
+                MessageBox.Show($"Tìm thấy khách hàng: {kh.HoTen}, {kh.GioiTinh}, {kh.DiaChi}, {kh.DienThoai}");
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy khách hàng!");
+            }
+        }
+
+        private void thoat_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Bạn có muốn lưu dữ liệu trước khi thoát?", "Xác nhận", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                SaveData();  
+            }
+
+            this.Close();
+        }
+       
+
+
+    }
+}
